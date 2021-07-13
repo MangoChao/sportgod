15877aa607daec8b5d98c4aa7d5ea66519728510
MangoChao <inm920901@gmail.com>
2021-07-13 08:47:12 +0800

    no message

---
+++ application/index/controller/Article.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\index\controller;
+
+use app\common\controller\Frontend;
+use think\Log;
+
+//文章
+class Article extends Frontend
+{
+
+    protected $noNeedLogin = '*';
+    protected $noNeedRight = '*';
+    protected $noNeedMerchant = '*';
+    protected $layout = 'base';
+
+    //文章列表
+    public function index()
+    {
+        return $this->view->fetch();
+    }
+    
+    //文章內容
+    public function detail($id = null)
+    {
+        return $this->view->fetch();
+    }
+    
+
+}
