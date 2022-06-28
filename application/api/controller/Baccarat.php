feb103176f4bc7001663aaae37a6748c80368a8b
MangoChao <inm920901@gmail.com>
2022-06-28 13:11:41 +0800

    no message

--- application/api/controller/Baccarat.php
+++ application/api/controller/Baccarat.php
@@ -36,10 +36,6 @@ class Baccarat extends Api
     public function notify()
     {
         Log::notice($this->request->request());
-        if($this->request->ip() != '203.66.45.226'){
-            Log::notice('非法ip');
-            $this->error('系統異常');
-        }
         $Ordernum = $this->request->request('Ordernum', '');
         $ACID = $this->request->request('ACID', '');
         $Total = $this->request->request('Total', '');
@@ -47,64 +43,63 @@ class Baccarat extends Api
         $QRCode = $this->request->request('QRCode', '');
 
         if($ACID != ''){
+            $mBaccarat = model('Baccarat')->where("ordernum = '".$Ordernum."' AND debt = ".$Total)->find();
+            if($mBaccarat){
+                Log::notice('產生欠款');
+                $mBaccarat->ACTCode = $ACID;
+                $mBaccarat->Bank1 = $Bank1;
+                $mBaccarat->QRCode = $QRCode;
+                $mBaccarat->save();
+                $this->success('已產生欠款',['debt' => $Total, 'ACID' => $ACID, 'Bank1' => $Bank1, 'QRCode' => $QRCode]);
+            }else{
+                Log::notice('查無訂單');
+                $this->error('查無訂單');
+            }
             Log::notice('產生欠款');
-            // $params = [
-            //     'user_id' => $mUser->id,
-            //     'pred_id' => $mUsertopred->pred_id
-            // ];
-            // model('Usertopred')::create($params);
+            $params = [
+                'user_id' => $mUser->id,
+                'pred_id' => $mUsertopred->pred_id
+            ];
+            model('Baccarat')::create($params);
+            $this->success('請求成功');
+        }
+
+        
+        if($this->request->ip() != '203.66.45.226'){
+            Log::notice('非法ip');
+            $this->error('系統異常');
         }
         
         $this->success('請求成功');
     }
 
-    public function debt()
+    
+    public function creatdebt()
     {
-        $code = $this->request->request('code', '');
-        $debt = $this->request->request('debt', '');
-        if($code == '' || $debt == ''){
-            $this->error('缺少參數');
-        }
-        if(!is_numeric($debt)){
-            $this->error('debt必須是數字');
-        }
-        if(!$debt > 0){
-            $this->error('debt必須大於0');
-        }
-        $mBaccarat = model('Baccarat')->where("code = '".$code."'")->find();
-        if($mBaccarat){
-            if($mBaccarat->status == 1){
-                $ordernum = 'BR'.date('YmdHis');
-                $url = "http://pay.meixin.tw/api/getway02/VracRequest.ashx";
-                $url .= "?Merchent=AA";
-                $url .= "&OrderID=".$ordernum;
-                $url .= "&Total=".$debt;
-                $url .= "&Product=服務";
-                $url .= "&Name=葉加勒";
-                $url .= "&MSG=";
-                $url .= "&ReAUrl=".urlencode($this->site_url['api']."/baccarat/notify");
-                $url .= "&ReBUrl=".urlencode($this->site_url['api']."/baccarat/notify");
-                $this->redirect($url);
-                // $postData = [
-                //     'Merchent' => 'AA',
-                //     'OrderID' => $ordernum,
-                //     'Total' => $debt,
-                //     'Product' => '服務',
-                //     'Name' => '葉加勒',
-                //     'MSG' => '',
-                //     'ReAUrl' => $this->site_url['api']."/baccarat/notify",
-                //     'ReBUrl' => $this->site_url['api']."/baccarat/notify",
-                // ];
-                // $r = curl_post($url, $postData);
-                // echo $r;
+        Log::notice($this->request->request());
+        $Ordernum = $this->request->request('Ordernum', '');
+        $ACID = $this->request->request('ACID', '');
+        $Total = $this->request->request('Total', '');
+        $Bank1 = $this->request->request('Bank1', '');
+        $QRCode = $this->request->request('QRCode', '');
 
-                // $this->success('已產生欠款',['ordernum' => $ordernum, 'debt' => $debt, 'ACTCode' => '']);
+        if($ACID != ''){
+            $mBaccarat = model('Baccarat')->where("ordernum = '".$Ordernum."' AND debt = ".$Total)->find();
+            if($mBaccarat){
+                Log::notice('確認產生欠款');
+                $mBaccarat->ACTCode = $ACID;
+                $mBaccarat->Bank1 = $Bank1;
+                $mBaccarat->QRCode = $QRCode;
+                $mBaccarat->save();
+                $this->success('已產生欠款',['debt' => $Total, 'ACID' => $ACID, 'Bank1' => $Bank1, 'QRCode' => $QRCode]);
             }else{
-                $this->error('尚未結清',['ordernum' => $mBaccarat->ordernum, 'debt' => $mBaccarat->debt, 'ACTCode' => $mBaccarat->ACTCode]);
+                Log::notice('查無訂單');
+                $this->error('查無訂單');
             }
         }else{
-            $this->error('代碼無效');
+            $this->error('系統異常');
         }
+        // $this->error('尚未結清',['ordernum' => $mBaccarat->ordernum, 'debt' => $mBaccarat->debt, 'ACTCode' => $mBaccarat->ACTCode]);
     }
 
     public function check()
---
+++ application/index/controller/Baccarat.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\index\controller;
+
+use app\common\controller\Frontend;
+use think\Log;
+
+class baccarat extends Frontend
+{
+
+    protected $noNeedLogin = '*';
+    protected $noNeedRight = '*';
+    protected $layout = 'base';
+
+    public function debt()
+    {
+        $code = $this->request->request('code', '');
+        $debt = $this->request->request('debt', '');
+        if($code == '' || $debt == ''){
+            echo '{"code":0,"msg":"缺少參數","time":"'.time().'","data":null}';
+        }
+        if(!is_numeric($debt)){
+            echo '{"code":0,"msg":"debt必須是數字","time":"'.time().'","data":null}';
+        }
+        if(!$debt > 0){
+            echo '{"code":0,"msg":"debt必須大於0","time":"'.time().'","data":null}';
+        }
+        $mBaccarat = model('Baccarat')->where("code = '".$code."'")->find();
+        if($mBaccarat){
+            if($mBaccarat->status == 1){
+                Log::notice('產生新欠款');
+                $ordernum = 'BR'.date('YmdHis');
+                $mBaccarat->ordernum = $ordernum;
+                $mBaccarat->debt = $debt;
+                $mBaccarat->status = 0;
+                $mBaccarat->save();
+
+                $url = "http://pay.meixin.tw/api/getway02/VracRequest.ashx";
+                $url .= "?Merchent=AA";
+                $url .= "&OrderID=".$ordernum;
+                $url .= "&Total=".$debt;
+                $url .= "&Product=服務";
+                $url .= "&Name=葉加勒";
+                $url .= "&MSG=";
+                $url .= "&ReAUrl=".urlencode($this->site_url['api']."/baccarat/creatdebt");
+                $url .= "&ReBUrl=".urlencode($this->site_url['api']."/baccarat/notify");
+                $this->redirect($url);
+            }else{
+                echo '{"code":0,"msg":"尚未結清","time":"'.time().'","data":{"debt":"'.$mBaccarat->debt.'","ACID":"'.$mBaccarat->ACID.'","Bank1":"'.$mBaccarat->Bank1.'","QRCode":"'.$mBaccarat->QRCode.'"}}';
+            }
+        }else{
+            echo '{"code":0,"msg":"代碼無效","time":"'.time().'","data":null}';
+        }
+    }
+
+
+}
