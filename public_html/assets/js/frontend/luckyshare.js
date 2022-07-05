70a24dd2be111ae4debb26bbe57dbdea9644bbf1
MangoChao <inm920901@gmail.com>
2022-07-05 19:02:49 +0800

    no message

--- public_html/assets/js/frontend/luckyshare.js
+++ public_html/assets/js/frontend/luckyshare.js
@@ -8,6 +8,10 @@ define(['jquery', 'bootstrap', 'frontend', 'form', 'template'], function ($, und
     };
     var Controller = {
         index: function () {
+            layer.photos({
+                photos: '.luckyshare_list'
+                ,anim: 5 //0-6的选择，指定弹出图片动画类型，默认随机（请注意，3.0之前的版本用shift参数）
+              }); 
         },
         add: function () {
             Form.api.bindevent($("#luckyshare_form"), function (data) {
