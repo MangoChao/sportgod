03662e7b858ceca9c630fe29a51b36f17e6b2890
MangoChao <inm920901@gmail.com>
2022-07-27 14:01:50 +0800

    no message

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -18,7 +18,7 @@ class Index extends Frontend
         $mNewArticle = model('Godarticle')->alias('a')
         ->join("article_cat ac","ac.id = a.cat_id AND ac.status = 1")
         ->field('a.*')
-        ->where("a.status = 1 AND a.cover_img <> '' ")->order("a.createtime","desc")->limit(7)->select();
+        ->where("a.status = 1 AND a.cover_img <> '' ")->order("a.createtime","desc")->limit(8)->select();
 
         //棒球
         $mArticle1 = model('Godarticle')->alias('a')
