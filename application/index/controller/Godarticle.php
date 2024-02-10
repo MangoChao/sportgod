010b88cd9065ce8bffcfcb6a5c7df912fdebe7e5
Chao <inm20901@yahoo.com.tw>
2024-02-10 19:29:32 +0800

    no message

--- application/index/controller/Godarticle.php
+++ application/index/controller/Godarticle.php
@@ -18,35 +18,58 @@ class Godarticle extends Frontend
     }
 
     //文章列表
-    public function index($id=null)
+    public function index($id=null, $cat = null)
     {
         $whereStr = "";
         if($id){
-            $whereStr = " AND gt.id = '".$id."'";
+            $whereStr .= " AND gt.id = '".$id."'";
+            if($id != 2){
+                $whereStr .= " AND ac.status = 1";
+            }
+        }
+        if($cat){
+            $whereStr .= " AND ac.id = '".$cat."'";
         }
         $page = $this->request->request('page', 1);
+        $paginate = 9999;
         $mGodarticle = model('Godarticle')->alias('a')
         ->join("user u","(u.id = a.user_id AND u.status = 1) OR a.user_id = 0 ")
         ->join("god_type gt","gt.id = a.god_type and gt.status = 1")
-        ->field('a.*, u.nickname, u.avatar')
-        ->where("a.status = 1 ".$whereStr)->group('a.id')->order('a.updatetime','desc')->paginate(20, false, $this->paginate_config);
+        ->join("article_cat ac","ac.id = a.cat_id")
+        ->field('a.*, u.nickname, u.avatar, ac.cat_name')
+        ->where("a.status = 1 AND (a.cover_img <> '' OR a.video_url <> '' ) ".$whereStr)->group('a.id')->order(['ac.weigh' => 'asc', 'a.updatetime' => 'desc'])->paginate($paginate, false, $this->paginate_config);
         // Log::notice( model('Godarticle')->getLastSql());
         
-        // if($mGodarticle){
-        //     foreach($mGodarticle as $v){
-        //         // if(!$v->avatar) $v->avatar = $this->def_avatar;
-        //     }
-        // }
+        $list = [];
+        if($mGodarticle){
+            $teachCatList = getTeachCatList();
+            foreach($mGodarticle as $v){
+                if (!isset($list[$v->cat_id])){
+                    if($id == 2){
+                        $title = $teachCatList[$v->cat_id];
+                    }else{
+                        $title = $v->cat_name;
+                    }
+                    $list[$v->cat_id] = [
+                        'title' => $title,
+                        'list' => []
+                    ];
+                }
+
+                $list[$v->cat_id]['list'][] = $v;
+                // if(!$v->avatar) $v->avatar = $this->def_avatar;
+            }
+        }
 
         $count = $mGodarticle->total();
         $pagelist = $mGodarticle->render();
         
         
-        //1神人 2教學 3新聞 4視界
         $this->view->assign('count', $count);
         $this->view->assign('page', $page);
         $this->view->assign('pagelist', $pagelist);
         $this->view->assign('mGodarticle', $mGodarticle);
+        $this->view->assign('list', $list);
         
         //1神人 2教學 3新聞 4視界
         if($id == 2){
@@ -64,15 +87,41 @@ class Godarticle extends Frontend
     {
         $mGodarticle = model('Godarticle')->alias('a')
         ->join("user u","(u.id = a.user_id AND u.status = 1) OR a.user_id = 0 ")
-        ->field('a.*, u.nickname, u.avatar')
+        ->join("article_cat ac","ac.id = a.cat_id")
+        ->field('a.*, u.nickname, u.avatar, ac.cat_name')
         ->where("a.status = 1 AND a.id = ".$id)->find();
 
         if(!$mGodarticle){
             $this->redirect('/index/godarticle');
         }
+        $mGodarticle->views += 1; 
+        $mGodarticle->save();
         if(!$mGodarticle->avatar) $mGodarticle->avatar = $this->def_avatar;
 
         $this->view->assign('mGodarticle', $mGodarticle);
+
+        $teachCatList = getTeachCatList();
+        if($mGodarticle->god_type == 2){
+            $title = $teachCatList[$mGodarticle->cat_id];
+        }else{
+            $title = $mGodarticle->cat_name;
+        }
+        $list = [
+            'title' => $title,
+            'list' => []
+        ];
+        $mGodarticleList = model('Godarticle')->alias('a')
+        ->join("user u","(u.id = a.user_id AND u.status = 1) OR a.user_id = 0 ")
+        ->join("god_type gt","gt.id = a.god_type and gt.status = 1")
+        ->join("article_cat ac","ac.id = a.cat_id")
+        ->field('a.*, u.nickname, u.avatar, ac.cat_name')
+        ->where("a.status = 1 AND (a.cover_img <> '' OR a.video_url <> '' ) AND a.cat_id = ".$mGodarticle->cat_id." AND a.god_type = ".$mGodarticle->god_type." ")->group('a.id')->order(['ac.weigh' => 'asc', 'a.updatetime' => 'desc'])->select();
+        if($mGodarticleList){
+            foreach($mGodarticleList as $v){
+                $list['list'][] = $v;
+            }
+        }
+        $this->view->assign('list', $list);
 
         //1神人 2教學 3新聞 4視界
         if($mGodarticle->god_type == 2){
