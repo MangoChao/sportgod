378e086df104c10e30d87dc404a506cdb35297aa
Chao <pkh_chao@photons.com.tw>
2025-11-04 17:26:59 +0800

    no message

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -266,7 +266,8 @@ class Index extends Api
             'paction' => 'login-processing',
             'remember' => 1
         ];
-        $cookie = loginSetCookie($url, $post, 'hau888_cookie.txt');
+        // $cookie = loginSetCookie($url, $post, 'hau888_cookie.txt');
+        $cookie = getCookiePath('hau888_cookie.txt');
         $content = $this->get_content($gameurl.'/today_events_show_list.php?is_start=0&game_category=1', $cookie);
         Log::notice("----------content------------");
         Log::notice($content);
