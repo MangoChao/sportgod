8553ec8a8c325d5f677c18ca389340a30fe46b0c
Chao <inm20901@yahoo.com.tw>
2025-11-25 00:07:13 +0800

    no message

--- application/admin/controller/event/Linepredcode.php
+++ application/admin/controller/event/Linepredcode.php
@@ -139,7 +139,7 @@ class Linepredcode extends Backend
         }
         $max = 5;
         do{
-            $code = "##".strtolower(Random::alnum(10));
+            $code = "##".strtolower(Random::alnum(4));
             $max--;
         }while($this->model->get(['code' => $code]) && $max >= 0);
         $this->view->assign("code", $code);
